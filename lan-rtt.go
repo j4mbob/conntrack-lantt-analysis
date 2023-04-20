ea5a4b7f15f29505aa8fb1ee6fec367d1f223e73
Jamie <jamie.walmsley@ask4.com>
2023-04-20 19:55:22 +0100

    moved stdout buffer  and parser to seperate function

--- lan-rtt.go
+++ lan-rtt.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -80,7 +81,7 @@ func ArgParse(arguments *Args) {
 		fmt.Printf("loading JSON config: %s\n", *configPtr)
 		LoadConfig(*configPtr, arguments)
 	} else {
-		if (*sslcertPtr == "" || *sslkeyPtr == "") && *nosslPtr == false {
+		if (*sslcertPtr == "" || *sslkeyPtr == "") && !*nosslPtr {
 			fmt.Printf("SSL options missing. cant start\n")
 			os.Exit(1)
 		}
@@ -121,7 +122,6 @@ func LoadConfig(configFile string, arguments *Args) {
 
 func PollConntrack(arguments *Args) {
 
-	var regex = regexp.MustCompile(`^\[([0-9]+)\.([0-9]+) *\].*(SYN_RECV|ESTABLISHED) src=([^ ]+) dst=([^ ]+) sport=([^ ]+) dport=([^ ]+) src=([^ ]+) dst=([^ ]+) sport=([^ ]+) dport=([^ ]+) (?:\[ASSURED\] )?id=([0-9]+)$`)
 	args := "-E -e UPDATES -o timestamp,id --buffer-size 1064960 -p tcp --orig-src " + arguments.Network + " --mask-src " + arguments.Subnet
 
 	var ctx context.Context
@@ -144,48 +144,20 @@ func PollConntrack(arguments *Args) {
 		fmt.Printf("std out error")
 		log.Fatal(err)
 	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Printf("stderr error")
+		log.Fatal(err)
+	}
+
 	if err := cmd.Start(); err != nil {
 		fmt.Printf("start error")
 		log.Fatal(err)
 
 	}
 
-	EventMap := make(map[string]map[string]interface{})
-	DeviceFlows := map[string][]float64{}
-	Flows := make([]Flows, 10)
-
-	mux := &sync.Mutex{}
-
-	scanner := bufio.NewScanner(stdout)
-
-	// launch thread for constant average calculations
-	go ParseFlows(&Flows, DeviceFlows, arguments, mux)
-
-	for scanner.Scan() {
-		output := scanner.Text()
-		//fmt.Println(output)
-		matches := regex.FindAllStringSubmatch(output, -1)
-
-		for _, attr := range matches {
-			combineTimeStamps := attr[1] + "." + attr[2]
-			var timestamp float64
-			if s, err := strconv.ParseFloat(combineTimeStamps, 64); err == nil {
-				timestamp = s
-			}
-
-			// simple ring buffer queue for flow events
-			if len(Flows) == arguments.BufferSize {
-				mux.Lock()
-				Flows = Flows[1:]
-				mux.Unlock()
-
-			}
-
-			NewEvent(timestamp, attr[3], attr[4], attr[5], attr[6], attr[7], attr[8], attr[9], attr[10], attr[11], attr[12], EventMap, &Flows, DeviceFlows, arguments, mux)
-			//fmt.Println(EventMap)
-		}
-
-	}
+	EventParser(stdout, stderr, arguments)
 
 	if err := cmd.Wait(); err != nil {
 		fmt.Printf("wait error")
@@ -195,6 +167,109 @@ func PollConntrack(arguments *Args) {
 
 }
 
+func EventParser(stdout io.ReadCloser, stderr io.ReadCloser, arguments *Args) {
+
+	var regex = regexp.MustCompile(`^\[([0-9]+)\.([0-9]+) *\].*(SYN_RECV|ESTABLISHED) src=([^ ]+) dst=([^ ]+) sport=([^ ]+) dport=([^ ]+) src=([^ ]+) dst=([^ ]+) sport=([^ ]+) dport=([^ ]+) (?:\[ASSURED\] )?id=([0-9]+)$`)
+
+	EventMap := make(map[string]map[string]interface{})
+	DeviceFlows := map[string][]float64{}
+	Flows := make([]Flows, 10)
+
+	mux := &sync.Mutex{}
+
+	go ParseFlows(&Flows, DeviceFlows, arguments, mux)
+
+	scannerStdout := bufio.NewScanner(stdout)
+	scannerStderr := bufio.NewScanner(stderr)
+
+	go func() {
+		for scannerStdout.Scan() {
+			output := scannerStdout.Text()
+			matches := regex.FindAllStringSubmatch(output, -1)
+
+			for _, attr := range matches {
+				combineTimeStamps := attr[1] + "." + attr[2]
+				var timestamp float64
+				if s, err := strconv.ParseFloat(combineTimeStamps, 64); err == nil {
+					timestamp = s
+				}
+
+				// simple ring buffer to throw away oldest flow when we get to BufferSize
+				if len(Flows) == arguments.BufferSize {
+					mux.Lock()
+					Flows = Flows[1:]
+					mux.Unlock()
+
+				}
+				NewEvent(timestamp, attr[3], attr[4], attr[5], attr[6], attr[7], attr[8], attr[9], attr[10], attr[11], attr[12], EventMap, &Flows, DeviceFlows, arguments, mux)
+			}
+		}
+	}()
+
+	go func() {
+		for scannerStderr.Scan() {
+			output := scannerStderr.Text()
+			fmt.Println(output)
+		}
+	}()
+
+}
+
+func NewEvent(timestamp float64, pkttype string, origSrc string, origDst string, origSport string, origDport string, replySrc string, replyDst string, replySport string, replyDsport string, flow_id string, EventMap map[string]map[string]interface{}, flows *[]Flows, DeviceFlows map[string][]float64, arguments *Args, mux *sync.Mutex) {
+
+	if arguments.ConntrackStdOut {
+		fmt.Printf("[%f] %v %v %v %v %v %v %v %v %v %v\n", timestamp, pkttype, origSrc, origDst, origSport, origDport, replySrc, replyDst, replySport, replyDsport, flow_id)
+	}
+
+	var syn_timestamp float64
+	var ack_timestamp float64
+	var lanrtt float64
+
+	if pkttype == "SYN_RECV" {
+
+		EventMap[flow_id] = make(map[string]interface{})
+		EventMap[flow_id]["timestamp"] = timestamp
+		EventMap[flow_id]["type"] = pkttype
+		EventMap[flow_id]["origSrc"] = origSrc
+		EventMap[flow_id]["origDst"] = origDst
+		EventMap[flow_id]["origSport"] = origSport
+		EventMap[flow_id]["origDport"] = origDport
+		EventMap[flow_id]["replySrc"] = replySrc
+		EventMap[flow_id]["replyDst"] = replyDst
+		EventMap[flow_id]["replySport"] = replySport
+		EventMap[flow_id]["replyDsport"] = replyDsport
+
+	} else {
+
+		_, present := EventMap[flow_id]
+
+		if present {
+
+			if EventMap[flow_id]["origSrc"] == origSrc && EventMap[flow_id]["origDst"] == origDst && EventMap[flow_id]["origSport"] == origSport && EventMap[flow_id]["origDport"] == origDport && EventMap[flow_id]["replySrc"] == replySrc && EventMap[flow_id]["replyDst"] == replyDst && EventMap[flow_id]["replySport"] == replySport && EventMap[flow_id]["replyDsport"] == replyDsport {
+				ack_timestamp = timestamp
+				syn_timestamp = EventMap[flow_id]["timestamp"].(float64)
+
+				lanrtt = CalculateFlowRtt(syn_timestamp, ack_timestamp)
+
+				mux.Lock()
+				*flows = append(*flows, Flows{FlowID: flow_id, DeviceIP: origSrc, SynTimestamp: syn_timestamp, AckTimestamp: ack_timestamp, LanRTT: lanrtt})
+				mux.Unlock()
+				for _, flow := range *flows {
+					if flow.DeviceIP == origSrc {
+						mux.Lock()
+						DeviceFlows[origSrc] = append(DeviceFlows[origSrc], flow.LanRTT)
+						mux.Unlock()
+					}
+				}
+
+				// throw away matched event to avoid NewEventMap constantly growing with every new event
+				delete(EventMap, flow_id)
+
+			}
+		}
+	}
+}
+
 func ParseFlows(flows *[]Flows, DeviceFlows map[string][]float64, arguments *Args, mux *sync.Mutex) {
 
 	promMean, promHisto, promAgMean, promAgHisto, promDeviceCount := PromExporter(arguments.PromPort, arguments.SSLCert, arguments.SSLKey, arguments.NoSSL)
@@ -202,11 +277,10 @@ func ParseFlows(flows *[]Flows, DeviceFlows map[string][]float64, arguments *Arg
 	for {
 		time.Sleep(time.Duration(arguments.StatsPeriod) * time.Second)
 
-		// WORK OUT MEAN NOW FOR AGGREGATED DATA FLOWS IN DEVICEFLOWS
 		CalculateAverages(&flows, promMean, promHisto, arguments, mux)
-		CalculateAggregateAverages(DeviceFlows, promAgMean, promAgHisto, promDeviceCount, mux)
+		CalculateAggregateAverages(DeviceFlows, promAgMean, promAgHisto, promDeviceCount, arguments, mux)
+
 		mux.Lock()
-		fmt.Println(len(DeviceFlows))
 		for k := range DeviceFlows {
 			delete(DeviceFlows, k)
 		}
@@ -268,65 +342,6 @@ func PromExporter(promPort string, sslCert string, sslKey string, noSSL bool) (p
 
 }
 
-func NewEvent(timestamp float64, pkttype string, origSrc string, origDst string, origSport string, origDport string, replySrc string, replyDst string, replySport string, replyDsport string, flow_id string, EventMap map[string]map[string]interface{}, flows *[]Flows, DeviceFlows map[string][]float64, arguments *Args, mux *sync.Mutex) {
-
-	if arguments.ConntrackStdOut {
-		fmt.Printf("[%f] %v %v %v %v %v %v %v %v %v %v\n", timestamp, pkttype, origSrc, origDst, origSport, origDport, replySrc, replyDst, replySport, replyDsport, flow_id)
-	}
-
-	//_, present := EventMap[flow_id]
-
-	var syn_timestamp float64
-	var ack_timestamp float64
-	var lanrtt float64
-
-	//NewEventMap := make(map[string]interface{})
-
-	if pkttype == "SYN_RECV" {
-
-		EventMap[flow_id] = make(map[string]interface{})
-		EventMap[flow_id]["timestamp"] = timestamp
-		EventMap[flow_id]["type"] = pkttype
-		EventMap[flow_id]["origSrc"] = origSrc
-		EventMap[flow_id]["origDst"] = origDst
-		EventMap[flow_id]["origSport"] = origSport
-		EventMap[flow_id]["origDport"] = origDport
-		EventMap[flow_id]["replySrc"] = replySrc
-		EventMap[flow_id]["replyDst"] = replyDst
-		EventMap[flow_id]["replySport"] = replySport
-		EventMap[flow_id]["replyDsport"] = replyDsport
-
-	} else {
-
-		_, present := EventMap[flow_id]
-
-		if present {
-
-			if EventMap[flow_id]["origSrc"] == origSrc && EventMap[flow_id]["origDst"] == origDst && EventMap[flow_id]["origSport"] == origSport && EventMap[flow_id]["origDport"] == origDport && EventMap[flow_id]["replySrc"] == replySrc && EventMap[flow_id]["replyDst"] == replyDst && EventMap[flow_id]["replySport"] == replySport && EventMap[flow_id]["replyDsport"] == replyDsport {
-				ack_timestamp = timestamp
-				syn_timestamp = EventMap[flow_id]["timestamp"].(float64)
-
-				lanrtt = CalculateFlowRtt(syn_timestamp, ack_timestamp)
-				//fmt.Println(lanrtt)
-				mux.Lock()
-				*flows = append(*flows, Flows{FlowID: flow_id, DeviceIP: origSrc, SynTimestamp: syn_timestamp, AckTimestamp: ack_timestamp, LanRTT: lanrtt})
-				mux.Unlock()
-				for _, flow := range *flows {
-					if flow.DeviceIP == origSrc {
-						mux.Lock()
-						DeviceFlows[origSrc] = append(DeviceFlows[origSrc], flow.LanRTT)
-						mux.Unlock()
-					}
-				}
-
-				// throw away matched event to avoid NewEventMap constantly growing with every new event
-				delete(EventMap, flow_id)
-
-			}
-		}
-	}
-}
-
 func CalculateFlowRtt(syn_timestamp float64, ack_timestamp float64) float64 {
 	delayTime := (ack_timestamp - syn_timestamp) * 1000
 	return delayTime
@@ -348,32 +363,31 @@ func CalculateAverages(flows **[]Flows, promMean prometheus.Gauge, promHisto pro
 
 	mean = delayTotal / float64(flowCount)
 	promMean.Set(mean)
-	//promHisto.Observe(mean)
 
 	if arguments.StatsStdOut {
 
-		fmt.Printf("\ndelayTotal: %f", delayTotal)
-		fmt.Printf("\nflowcount: %d", flowCount)
-		fmt.Printf("\nlan-rtt: %f\n", mean)
+		fmt.Printf("\nAll Flows: [delayTotal: %f] [Flowcount: %d] [LAN Rtt: %f]\n", delayTotal, flowCount, mean)
 	}
 }
 
-func CalculateAggregateAverages(DeviceFlows map[string][]float64, promAgMean prometheus.Gauge, promAgHisto prometheus.Histogram, promDeviceCount prometheus.Gauge, mux *sync.Mutex) {
+func CalculateAggregateAverages(DeviceFlows map[string][]float64, promAgMean prometheus.Gauge, promAgHisto prometheus.Histogram, promDeviceCount prometheus.Gauge, arguments *Args, mux *sync.Mutex) {
 	var devicesCount int
 	var devicesMean float64
-	//var mean float64
+
 	mux.Lock()
 	for _, v := range DeviceFlows {
 		devicesMean += CalculateMean(v)
 		promAgHisto.Observe(CalculateMean(v))
-		//fmt.Printf("IP: %v Flows: %v LanRtt: %v\n", k, len(v), CalculateMean(v))
 	}
 
 	devicesCount = len(DeviceFlows)
 	promDeviceCount.Set(float64(devicesCount))
 
 	AggregatedMean := devicesMean / float64(devicesCount)
-	fmt.Printf("Total Device Count: %v Aggregated Mean: %v\n", devicesCount, AggregatedMean)
+
+	if arguments.StatsStdOut {
+		fmt.Printf("Aggregated Per Device: [Total Device Count: %v] [Aggregated Mean LAN Rtt: %v]\n", devicesCount, AggregatedMean)
+	}
 
 	promAgMean.Set(AggregatedMean)
 	mux.Unlock()
